972e0bb7a00351830436bd7ea3bfc779a3e50bb7
Hecmocer <hmoreno@ruc.dk>
2016-03-08 21:13:24 +0100

    navbar collapses properly

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-//window.scrollY
+// Para definir el alto de la navbar
+//$('.web.header')[0].offsetHeight = 10;
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 20) {
-    console.log('add Shrink');
-    $('.web.header').addClass('shrink');
-  } else {
-    $('.web.header').removeClass('shrink');
-    console.log('remove Shrink');
-  }
+// Para definir el scroll vertical
+//$(document)[0].scrollingElement.scrollTop = 0;
+
+
+$(window).on('scroll', function() {
+    console.log($(document)[0].scrollingElement.scrollTop);
+
+    if ($(document).scrollTop() < 180) {
+        console.log('add hide');
+
+        $('.web.header.shrinked').addClass('hide');
+    }
+    else{
+        $('.web.header.shrinked').removeClass('hide');
+        console.log('added hide');
+    }
 });

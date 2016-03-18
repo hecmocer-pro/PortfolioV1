6b0006e763f480ea96144e940fa9150189418e1a
Héctor <hecmocer@gmail.com>
2016-03-18 13:08:21 +0100

    Mejoradas las imagenes e intentado hacer un loader, pero no funciona asi que no está incluido

--- js/app.js
+++ js/app.js
@@ -67,13 +67,11 @@ $(window).on('scroll', function() {
     console.log($(document)[0].scrollingElement.scrollTop);
 
     if ($(document).scrollTop() < 130) {
-        console.log('add hide');
         $('.web.header.shrinked').addClass('hide');
         $('.web.header.shrinked').removeClass('show');
         $('.header.title.shrinked').removeClass('bounceInDown');
     }
     else{
-        console.log('added hide');
         $('.web.header.shrinked').removeClass('hide');
         $('.web.header.shrinked').addClass('show');
         $('.header.title.shrinked').addClass('bounceInDown');
